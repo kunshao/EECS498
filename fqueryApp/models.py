ff6cda309235e2c56d9181911452ada243160b4c
Shu Lin <linshu123@gmail.com>
2014-04-05 18:57:20 -0400

    retrieve and display comments for statuses

--- fqueryApp/models.py
+++ fqueryApp/models.py
@@ -9,3 +9,17 @@ class Status(models.Model):
 
     def __unicode__(self):
         return self.status_message
+
+
+class Comment(models.Model):
+    comment_id              = models.CharField(max_length = 30, primary_key = True)
+    comment_from_id         = models.CharField(max_length = 30, blank=True)
+    comment_message         = models.TextField(blank = True)
+    comment_can_remove      = models.NullBooleanField(null = True)
+    comment_created_time    = models.DateTimeField(blank = True, null = True)
+    comment_like_count      = models.IntegerField(null = True)
+    user_likes              = models.NullBooleanField(null = True)
+
+    def __unicode__(self):
+        return self.comment_message
+
--- fqueryApp/views.py
+++ fqueryApp/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import simplejson
 
-from fqueryApp.models import Status
+from fqueryApp.models import Status, Comment
 from fquery import settings
 
 
