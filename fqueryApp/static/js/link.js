2bf97f78fd0a3877d6c48e32255eb8a096cdee97
Shu Lin <linshu123@gmail.com>
2014-04-13 14:35:04 -0400

    fixed minor things. now Facebook data doesn't show up on the page. integrated some code from index to home page

--- fqueryApp/static/js/link.js
+++ fqueryApp/static/js/link.js
@@ -8,27 +8,27 @@ function get_partial_link_list(limit, offset){
     log("Fetching new page...");
     query = 'me/links?limit=' + limit + '&offset=' + offset;
     FB.api(query, function (response) {
-            link_list = response.data;
+        link_list = response.data;
 
-            if (link_list.length > 0){
+        if (link_list.length > 0){
             window.link_count += link_list.length;
             save_links(link_list);
             get_partial_link_list(limit, offset + limit)
-            }
-            else{
+        }
+        else{
             document.getElementById("link_list_length").innerHTML = window.link_count + " links retrieved."
-            }
+        }
 
-            })
+    })
 }
 
 
 function save_links (list) {
     $.post(
-            "{% url 'fqueryApp:save_links' %}",
-            JSON.stringify(list),
-            function(server_response) {
+        "{% url 'fqueryApp:save_links' %}",
+        JSON.stringify(list),
+        function(server_response) {
             log(server_response);
-            }
-          );
+        }
+        );
 }
--- fqueryApp/static/js/status.js
+++ fqueryApp/static/js/status.js
@@ -9,33 +9,33 @@ function get_partial_status_list(limit, offset){
     log("Fetching new page...");
     query = 'me/statuses?limit=' + limit + '&offset=' + offset;
     FB.api(query, function (response) {
-            status_list = response.data;
+        status_list = response.data;
 
-            if (status_list.length > 0){
+        if (status_list.length > 0){
             window.status_count += status_list.length;
-            var status_list_ul = document.createElement("ul");
-            for (var i = 0; i < status_list.length; ++i){
+            // var status_list_ul = document.createElement("ul");
+            // for (var i = 0; i < status_list.length; ++i){
 
-            var status_li = document.createElement("li");
-            status_li.innerHTML = status_list[i].message;
-            if (status_list[i].comments){
-            log("Making comment list...");
-            status_li.appendChild(make_comment_list(status_list[i].comments));
-            }
-            else{
-            log("No comment list...");
-            }
-            status_list_ul.appendChild(status_li);
-            }
+            //     var status_li = document.createElement("li");
+            //     status_li.innerHTML = status_list[i].message;
+            //     if (status_list[i].comments){
+            //         log("Making comment list...");
+            //         status_li.appendChild(make_comment_list(status_list[i].comments));
+            //     }
+            //     else{
+            //         log("No comment list...");
+            //     }
+            //     status_list_ul.appendChild(status_li);
+            // }
 
-            document.getElementById("status_list_div").appendChild(status_list_ul)
-                save_statuses(status_list);
+            // document.getElementById("status_list_div").appendChild(status_list_ul)
+            save_statuses(status_list);
             get_partial_status_list(limit, offset + limit)
-            }
-            else{
-                document.getElementById("status_list_length").innerHTML =
-                    window.status_count + " statuses retrieved."
-            }
+        }
+        else{
+            document.getElementById("status_list_length").innerHTML =
+            window.status_count + " statuses retrieved."
+        }
 
     })
 }
