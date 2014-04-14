f4ac42267db8e9a8977a9ab22e3a9a92749e6c12
Kun Shao <kunshao@umich.edu>
2014-04-13 21:53:58 -0400

    Modularized search.py
    plus deleted some garbage comments

--- fqueryApp/search.py
+++ fqueryApp/search.py
@@ -1,25 +1,60 @@
-# import os
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fquery.settings")
-# from django.conf import settings
-
+from collections import defaultdict 
 from fquery import settings
-
-from models import Status
+from models import Status, Comment, Photo, Link, Note, Video, Post
 import queryProcess, index
 
-from collections import defaultdict 
+
+CONTENT_TYPE_STATUS         = 1
+CONTENT_TYPE_COMMENT        = 1 << 1
+CONTENT_TYPE_LINK           = 1 << 2
+CONTENT_TYPE_PHOTO          = 1 << 3
+CONTENT_TYPE_NOTE           = 1 << 4
+CONTENT_TYPE_POST           = 1 << 5
+CONTENT_TYPE_VIDEO          = 1 << 6
+CONTENT_TYPE_QUESTION       = 1 << 7
+CONTENT_TYPE_QUESTION_OPTION = 1 << 8
+
 
 stopwords = queryProcess.importStopwords()
 
-def searchStatuses(query):
+def get_tokens(content_type):
+    # =========== Extract Token Set ============ #
+    # query_set = Status.objects.none()
+    # for word in query.split():
+    #     if word in stopwords:
+    #         continue
+    #     word = queryProcess.stemword(word)
+    #     query_set = query_set| Status.objects.filter(status_message__contains = word)
+
+    tokens_lst = defaultdict(dict)
+    num_docs = 0
+
+    if ((content_type & CONTENT_TYPE_STATUS) == CONTENT_TYPE_STATUS):
+        status_all = Status.objects.all()
+        num_docs  = Status.objects.count()
+        for status in status_all:
+            tokens = queryProcess.processLine(status.status_message)
+            for token in tokens:
+                tokens_lst[token][status.status_id] = tokens_lst.get(token, {}).get(status.status_id, 0) + 1
+
+    return [tokens_lst, num_docs]
+
+def get_results(doc_set, content_type):
 
     results = []
 
-    # =========== Extract Token Set ============ #
+    if ((content_type & CONTENT_TYPE_STATUS) == CONTENT_TYPE_STATUS):
+        for doc_no in sorted(doc_set, key = doc_set.get, reverse= True):
+            statuses = Status.objects.filter(status_id = str(doc_no))
+            for status in statuses:
+                results.append(status.status_message)
 
-    # This only applies to Status
-    status_all = Status.objects.all()
-    num_files  = Status.objects.count()
+    return results
+
+def searchStatuses(query):
+
+    # for testing purpose
+    content_type = CONTENT_TYPE_STATUS
 
     # tokens_lst is a dictionary of token and its occurrences in document
     # Each word is mapped to a list of (document number, document frequency) pair
@@ -27,14 +62,11 @@ def searchStatuses(query):
     # its entry in tokens_lst would be ['A': (1,1), (4,2), (9,1)]
 
     tokens_lst = defaultdict(dict)
-    for status in status_all:
-        tokens = queryProcess.processLine(status.status_message)
-        for token in tokens:
-            tokens_lst[token][status.status_id] = tokens_lst.get(token, {}).get(status.status_id, 0) + 1
+
+    [tokens_lst, num_docs] = get_tokens(content_type)
 
     # eliminate stopwords and stemming
     tokens_lst = queryProcess.stemmer(tokens_lst, stopwords)
-
 
     # ===== Applying weighting scheme ===== #
 
@@ -43,7 +75,7 @@ def searchStatuses(query):
     for token, doc_list in tokens_lst.items(): 
         doc_freq_lst[token] = len(doc_list)
 
-    weight_index = index.calcWeight(tokens_lst, num_files)
+    weight_index = index.calcWeight(tokens_lst, num_docs)
     doc_length = index.calcDocLen(weight_index)
 
 
@@ -74,7 +106,7 @@ def searchStatuses(query):
                 doc_set[doc_no] = 0
 
     # calculate term weight and query length
-    query_weight_index = index.calcQueryWeight(doc_freq_lst, query_tokens_lst, num_files)
+    query_weight_index = index.calcQueryWeight(doc_freq_lst, query_tokens_lst, num_docs)
     query_length = index.calcDocLen(query_weight_index)
             
     # Calculate the cosine similarity
@@ -90,21 +122,10 @@ def searchStatuses(query):
         doc_set[doc_no] = doc_set[doc_no]/(doc_length[doc_no] * query_length[query_doc_no])
 
 
+    results = get_results(doc_set, content_type)
 
-    for doc_no in sorted(doc_set, key = doc_set.get, reverse= True):
-
-        # This only applies to Status
-        statuses = Status.objects.filter(status_id = str(doc_no))
-        for status in statuses:
-            results.append(status.status_message)
-            # print result.status_message
 
     return results
-    # query_set = Status.objects.none()
-    # for word in query.split():
-    #     if word in stopwords:
-    #         continue
-    #     word = queryProcess.stemword(word)
-    #     query_set = query_set| Status.objects.filter(status_message__contains = word)
+
 
 
