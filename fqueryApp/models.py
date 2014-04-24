4e6e2bd9e03a9a38c0e6f349a1629d4ac56c030f
Shu Lin <linshu123@gmail.com>
2014-04-23 23:50:18 -0400

    increased id field length to 100 for all models

--- fqueryApp/models.py
+++ fqueryApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Status(models.Model):
-    status_id               = models.CharField(max_length = 30, primary_key = True)
+    status_id               = models.CharField(max_length = 100, primary_key = True)
     owner_id                = models.CharField(max_length = 30, blank = False)
     status_from_id          = models.CharField(max_length = 30, blank=True)
     status_message          = models.TextField(blank = True)
@@ -13,7 +13,7 @@ class Status(models.Model):
 
 
 class Comment(models.Model):
-    comment_id              = models.CharField(max_length = 30, primary_key = True)
+    comment_id              = models.CharField(max_length = 100, primary_key = True)
     owner_id                = models.CharField(max_length = 30, blank = False)
     comment_from_id         = models.CharField(max_length = 30, blank=True)
     comment_message         = models.TextField(blank = True)
@@ -24,7 +24,7 @@ class Comment(models.Model):
 
 
 class Link(models.Model):
-    link_id                 = models.CharField(max_length = 30, primary_key = True)
+    link_id                 = models.CharField(max_length = 100, primary_key = True)
     owner_id                = models.CharField(max_length = 30, blank = False)
     link_created_time       = models.DateTimeField(blank = True, null = True)
     link_description        = models.TextField(blank = True)
@@ -38,7 +38,7 @@ class Link(models.Model):
         return self.link_link
 
 class Photo(models.Model):
-    photo_id                = models.CharField(max_length = 30, primary_key = True)
+    photo_id                = models.CharField(max_length = 100, primary_key = True)
     owner_id                = models.CharField(max_length = 30, blank = False)
     photo_created_time      = models.DateTimeField(blank = True, null = True)
     photo_from_id           = models.CharField(max_length = 30, blank=True)
@@ -49,7 +49,7 @@ class Photo(models.Model):
         return self.photo_name
 
 class Note(models.Model):
-    note_id                 = models.CharField(max_length = 30, primary_key = True)
+    note_id                 = models.CharField(max_length = 100, primary_key = True)
     owner_id                = models.CharField(max_length = 30, blank = False)
     note_created_time       = models.DateTimeField(blank = True, null = True)
     note_from_id            = models.CharField(max_length = 30, blank=True)
@@ -61,7 +61,7 @@ class Note(models.Model):
             return self.note_subject
 
 class Video(models.Model):
-    video_id                = models.CharField(max_length = 30, primary_key = True)
+    video_id                = models.CharField(max_length = 100, primary_key = True)
     owner_id                = models.CharField(max_length = 30, blank = False)
     video_created_time      = models.DateTimeField(blank = True, null = True)
     video_description       = models.TextField(blank = True)
@@ -91,7 +91,7 @@ class Post(models.Model):
             return self.post_message
 
 class Question(models.Model):
-    question_id             = models.CharField(max_length = 30, primary_key = True)
+    question_id             = models.CharField(max_length = 100, primary_key = True)
     owner_id                = models.CharField(max_length = 30, blank = False)
     question_created_time   = models.DateTimeField(blank = True, null = True)
     question_from_id        = models.CharField(max_length = 30, blank=True)
@@ -102,7 +102,7 @@ class Question(models.Model):
             return self.question_question
 
 class QuestionOption(models.Model):
-    question_option_id      = models.CharField(max_length = 30, primary_key = True)
+    question_option_id      = models.CharField(max_length = 100, primary_key = True)
     owner_id                = models.CharField(max_length = 30, blank = False)
     question_option_created_time = models.DateTimeField(blank = True, null = True)
     question_option_from_id = models.CharField(max_length = 30, blank=True)
